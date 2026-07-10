b814d11871df777dc3a1aaabcc552f78998fb4f8
SylvesterKwon <sylvesterkwon@gmail.com>
2026-07-10 13:38:14 +0900

    build: bump BitLSM (ddbe22550)

--- storage/rocksdb/rdb_bitlsm_smoke.cc
+++ storage/rocksdb/rdb_bitlsm_smoke.cc
@@ -13,19 +13,20 @@ namespace myrocks {
 bool rdb_bitlsm_smoke_check() {
   bit_lsm::BitLSMOptions opts;
   opts.attr_num = 2;
-  opts.attr_types = {bit_lsm::CONTINUOUS, bit_lsm::CATEGORICAL};
+  // a0: ORDERED double (AttrSpec defaults), a1: UNORDERED opaque bytes.
+  opts.attr_specs = {bit_lsm::AttrSpec(bit_lsm::ORDERED),
+                     bit_lsm::AttrSpec(bit_lsm::UNORDERED)};
   opts.read_seqno = 0;
   opts.rho = 0.1;
 
   auto factory = std::make_shared<bit_lsm::SABIFactory>(opts);
 
   bit_lsm::BitLSMQuery query(std::vector<bit_lsm::OrClause>{
-      bit_lsm::OrClause{
-          {0, bit_lsm::CompareOp::GREATER_EQUAL, 10.5}},
-      bit_lsm::OrClause{
-          {1, bit_lsm::CompareOp::EQUAL, std::string("apple")}}});
+      bit_lsm::OrClause{{0, bit_lsm::CompareOp::GREATER_EQUAL, 10.5}},
+      bit_lsm::OrClause{{1, bit_lsm::CompareOp::EQUAL, std::string("apple")}}});
 
-  return factory != nullptr && query.clause_groups.size() == 2;
+  return factory != nullptr && query.clause_groups.size() == 2 &&
+         query.Validate(opts).ok();
 }
 
 }  // namespace myrocks
